ce1646d62c16dd6ca3971c38600dea6870692324
taynarodrigues <47439116+taynarodrigues@users.noreply.github.com>
2019-09-13 19:32:41 -0400

    Aula de Diagrama de Classe
    Astah
    Objetivo->  UML-> Gerar Código.

---
+++ ClassePessoa.php
@@ -0,0 +1,64 @@
+<?php
+ class Pessoa{
+	private $nome;
+	private $cpf;
+	private $data_nascimento;
+	private $sexo;
+	private $altura;
+	private $peso;
+	private $cor_da_pele;
+ }
+ //Método set 
+	function setNome($nome){
+		this nome -> $nome;
+	}
+	
+//Método get
+		function getNome($Nome){
+			this nome -> $nome;
+		}
+//CPF
+		function setCpf($cpf){
+			this cpf -> $cpf;
+	}
+		function getCpf($Cpf){
+			this cpf -> $cpf;
+		}
+//DATA DE NASCIMENTO
+		function setData_Nascimento($data_nascimento){
+			this data_nascimento -> $data_nascimento;
+		}
+		function getData_Nascimento($data_nascimento){
+			this data_nascimento -> $data_nascimento;
+		}
+//SEXO
+		function setSexo($sexo){
+			this sexo -> $sexo;
+		}
+		function getSexo($sexo){
+			this sexo -> $sexo;
+		}
+//ALTURA
+		function setAltura($altura){
+			this altura -> $altura;
+		}
+		function getAltura($altura){
+			this altura -> $altura;
+		}
+//PESO
+		function setPeso($peso){
+			this peso -> $peso;
+		}
+		function getPeso($peso){
+			this peso -> $peso;
+		}
+//COR DA PELE
+	
+		function setCor_da_pelee($cor_da_pele){
+			this cor_da_pele -> $cor_da_pele;
+		}
+		function getCor_da_pele($cor_da_pele){
+			this cor_da_pele -> $cor_da_pele;
+		}
+		
+?>
